b057414a4fc1e29dff42f015db102c5c6bdc4dd9
Latani-09 <127345009+Latani-09@users.noreply.github.com>
2024-06-17 06:41:52 +0530

    navbar

---
+++ app/navbar.js
@@ -0,0 +1,12 @@
+export default function Navbar(){
+    return <div className="header">
+        <div className="navbar">
+        <div className="nav">About</div>
+        <div className="nav">Projects</div>
+        <div className="nav">Contact me</div>
+        </div>
+
+
+    </div>
+}
+/**took 30 min to buil from scratch */
